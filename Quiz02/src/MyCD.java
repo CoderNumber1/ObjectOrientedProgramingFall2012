fccd47ed82e6b51ebbb1c935c82397b41472c36a
Anthony James <karl.a.james@hotmail.com>
2012-08-29 22:42:56 -0500

    Had to remove excess comments from main class before submitting.

--- Quiz02/src/MyCD.java
+++ Quiz02/src/MyCD.java
@@ -1,12 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Anthony
- */
 public class MyCD {
     private double principal;
     private double annualRate;
